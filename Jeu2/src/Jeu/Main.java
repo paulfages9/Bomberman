c92361e36efcca4bb57b1be7a0bbee7be15ff370
paulfages9 <33320829+paulfages9@users.noreply.github.com>
2018-01-26 15:45:16 +0100

    Menu Paul 26/01/18

--- Jeu2/src/Jeu/Main.java
+++ Jeu2/src/Jeu/Main.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Fenetre fenetre = new Fenetre();
-
+        //Fenetre fenetre = new Fenetre();
+        Menu menu = new Menu();
     }
 }
